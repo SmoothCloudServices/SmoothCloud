11493a5d024703d57dbe9e23d0ae6903030eb4b8
ezTxmMC <eztxmmc@hotmail.com>
2024-05-07 20:14:24 +0200

    added mainconfig

--- smoothcloud-node/src/main/java/eu/smoothcloudservices/smoothcloud/node/server/NettyServer.java
+++ smoothcloud-node/src/main/java/eu/smoothcloudservices/smoothcloud/node/server/NettyServer.java
@@ -2,8 +2,7 @@ package eu.smoothcloudservices.smoothcloud.node.server;
 
 import eu.smoothcloudservices.smoothcloud.api.misc.NettyUtils;
 import eu.smoothcloudservices.smoothcloud.node.SmoothCloudNode;
-import eu.smoothcloudservices.smoothcloud.node.config.CloudConfig;
-import io.netty5.bootstrap.ServerBootstrap;
+import eu.smoothcloudservices.smoothcloud.node.config.MainConfig;
 import io.netty5.channel.EventLoopGroup;
 import io.netty5.channel.MultithreadEventLoopGroup;
 import io.netty5.util.concurrent.Future;
@@ -16,7 +15,7 @@ public final class NettyServer {
 
     public NettyServer() {
 
-        CloudConfig config = ((SmoothCloudNode) SmoothCloudNode.getInstance()).getConfig();
+        MainConfig config = ((SmoothCloudNode) SmoothCloudNode.getInstance()).getConfig();
 
         /*new ServerBootstrap()
                 .channelFactory(NettyUtils.getChannelFactory())
