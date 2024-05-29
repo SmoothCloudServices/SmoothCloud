fffb78cb69b63774bda17faa7d1f6b1087cbaacc
ezTxmMC <eztxmmc@hotmail.com>
2024-05-29 15:46:37 +0200

    Revert "optimized NetworkInfo"
    This reverts commit 45807051dee582ad558806636007a9611c33ce77.

--- smoothcloud-client/src/main/java/eu/smoothservices/smoothcloud/client/lib/info/NetworkInfo.java
+++ smoothcloud-client/src/main/java/eu/smoothservices/smoothcloud/client/lib/info/NetworkInfo.java
@@ -1,16 +1,26 @@
 package eu.smoothservices.smoothcloud.client.lib.info;
 
-import lombok.Getter;
-
-@Getter
 public class NetworkInfo {
-    private final String serverId;
-    private final String hostName;
-    private final int port;
+
+    private String serverId;
+    private String hostName;
+    private int port;
 
     public NetworkInfo(String serverId, String hostName, int port) {
         this.serverId = serverId;
         this.hostName = hostName;
         this.port = port;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public String getServerId() {
+        return serverId;
+    }
+
+    public int getPort() {
+        return port;
     }
 }
