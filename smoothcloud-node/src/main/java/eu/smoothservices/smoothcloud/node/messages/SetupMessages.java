8f4c540afe20a75d56d0a5635f84b485f615c202
Timberclaw <131182894+TimberclawDev@users.noreply.github.com>
2024-06-02 16:46:01 +0200

    Fixed Setup messages

--- smoothcloud-node/src/main/java/eu/smoothservices/smoothcloud/node/messages/SetupMessages.java
+++ smoothcloud-node/src/main/java/eu/smoothservices/smoothcloud/node/messages/SetupMessages.java
@@ -6,7 +6,7 @@ public class SetupMessages {
     public static final String PREFIX = JavaColor.apply("&9Smooth&bCloud&8-&2Setup &8» &7");
 
     public static final String EULA_ACCEPT = JavaColor.apply("&7Do you agree to the Mojang EULA (https://www.minecraft.net/en-us/eula)? Possible answers: yes, no");
-    public static final String EULA_ACCEPTED = JavaColor.apply("&aYou has been accepted the Mojang EULA.");
+    public static final String EULA_ACCEPTED = JavaColor.apply("&aYou have accepted the Mojang EULA.");
     public static final String EULA_NOT_ACCEPTED = JavaColor.apply("&cPlease answer with yes or no!");
 
     public static final String CHOOSE_IP = JavaColor.apply("&7Which IP should we use?");
@@ -17,7 +17,7 @@ public class SetupMessages {
     public static final String CHOOSE_PORT = JavaColor.apply("&7Which port should we use?");
     public static final String CHOOSE_PORT_NOT_EXISTS = JavaColor.apply("&cPort not available. Please choose another port!");
 
-    public static final String CHOOSE_MEMORY = JavaColor.apply("&7How many memory should use the cloud?");
+    public static final String CHOOSE_MEMORY = JavaColor.apply("&7How much memory should use the cloud?");
     public static final String WRONG_MEMORY = JavaColor.apply("&cYou have to write a number or you need to add equals or more than 2gb");
 
     public static final String SAVED = JavaColor.apply("&aIP address, port and memory has been successfully saved.");
